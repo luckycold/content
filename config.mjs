91b6351a3ea90b911b3924e8c3c5c92df89462dc
luckycold <luke@luckycold.me>
2024-03-02 16:18:18 -0600

    Update config

--- config.mjs
+++ config.mjs
@@ -2,8 +2,14 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {
-  const config = {
-    showLinkPreviews: true,
-  };
+const config = {
+	title: "Luke's Lucky Room"
+	description: "This is a site to show off everything I've been learning",
+	author: "Luke",
+	logo: "/content/profile.png"
+	domain: "https://luckwill.win/",
+	navLink: [{href: "https://github.com/luckycold", name: "GitHub"}],
+	showLinkPreviews: true,
 };
+
+export default config; 
